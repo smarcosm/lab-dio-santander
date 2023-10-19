b6c20d3ee356d0f5d67cd4a97beca3c16cc63692
smarcosm <sebastiaomarcos49@gmail.com>
2023-10-19 15:56:29 -0300

    feat: :tada: Service criado

---
+++ src/main/java/com/smarcosm/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,31 @@
+package com.smarcosm.service.impl;
+
+import com.smarcosm.model.Cliente;
+import com.smarcosm.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
